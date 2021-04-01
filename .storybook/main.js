9ec4bab713b10c5a54e73dd6429fec13234b6c83
[object Object] <frytek97@gmail.com>
2021-04-01 17:43:44 +0200

    feat: add sass support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,25 @@
+const path = require("path");
+
 module.exports = {
-	stories: ["../src/**/*.stories.tsx"],
-	addons: [
-		"@storybook/addon-actions",
-		"@storybook/addon-links",
-	],
-	typescript: {
-		check: false,
-		checkOptions: {},
-		reactDocgen: 'react-docgen-typescript',
-		reactDocgenTypescriptOptions: {
-			shouldExtractLiteralValuesFromEnum: true,
-			propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-		},
-	},
+  stories: ["../src/**/*.stories.tsx"],
+  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.sass$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    return config;
+  },
 };
